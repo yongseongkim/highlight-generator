2d8d9e830a034402078e579a80d1d11fcf94b7c8
yongseongkim <kys911015@gmail.com>
2019-11-01 00:36:13 +0900

    add matplotlib

--- detector/highlight_detector.py
+++ detector/highlight_detector.py
@@ -6,13 +6,19 @@ from torch.utils.data.sampler import SubsetRandomSampler
 import numpy as np
 import copy
 import os
+import sys
 import time
+import matplotlib.pyplot as plt
 from models import HighlightDetector
 from dataset import HighlightDataset, ToTensor
 
 
 def train_model(model, dataloaders, criterion, optimizer, num_epochs=50):
     since = time.time()
+    hist = {
+        'train': [],
+        'val': []
+    }
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
@@ -48,25 +54,16 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=50):
                 running_loss += loss.item() * images.size(0)
 
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            hist[phase].append(epoch_loss)
             print('{} Loss: {:.4f}'.format(phase, epoch_loss))
         print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    return model
+    return model, hist
 
 
-if __name__ == "__main__":
-    curdirpath = os.path.dirname(os.path.realpath(__file__))
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    num_epochs = 100
-    features_dim = 64
-    hidden_dim = 32
-    layer_dim = 1
-    batch_size = 64
-    len_sequence = 7
-
+def load_dataset(len_sequence, batch_size, val_ratio=0.1):
     dataset = HighlightDataset(
         len_sequence=len_sequence,
         highlight_dir=os.path.join(
@@ -80,28 +77,95 @@ if __name__ == "__main__":
         ]))
     dataset_size = len(dataset)
     indices = list(range(dataset_size))
-    split = int(np.floor(0.1 * dataset_size))
+    split = int(np.floor(val_ratio * dataset_size))
     np.random.seed(dataset_size)
     np.random.shuffle(indices)
     train_indices, val_indices = indices[split:], indices[:split]
-
+    print('Dataset Size: {}, Train Dataset Size: {}, Val Dataset Size: {}'.format(
+        dataset_size, len(train_indices), len(val_indices)))
     dataloaders = {
         'train': torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_indices)),
         'val': torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(val_indices))
     }
+    return dataloaders
 
-    print('Dataset Size: {}, Train Dataset Size: {}, Val Dataset Size: {}'.format(dataset_size, len(train_indices), len(val_indices)))
+
+if __name__ == "__main__":
+    curdirpath = os.path.dirname(os.path.realpath(__file__))
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if len(sys.argv) < 2:
+        print('select \'train\' or \'run\'')
+        exit()
+
+    # model options
+    len_sequence = 7
+    features_dim = 128
+    hidden_dim = 32
+    layer_dim = 1
     model = HighlightDetector(features_dim=features_dim,
                               hidden_dim=hidden_dim,
                               layer_dim=layer_dim,
                               resnet_pretrained=True)
-    model = model.to(device)
     model_path = './highlight_detector_fd{}_hd{}_ld{}.ckpt'.format(str(features_dim),
                                                                    str(hidden_dim),
                                                                    str(layer_dim))
-    model_ft = train_model(model,
-                           dataloaders,
-                           nn.MSELoss(),
-                           torch.optim.Adam(model.parameters(), lr=0.001),
-                           num_epochs=num_epochs)
-    torch.save(model_ft.state_dict(), model_path)
+
+    option = sys.argv[1]
+    if option == 'train':
+        num_epochs = 100
+        if len(sys.argv) > 2:
+            num_epochs = int(sys.argv[2])
+        batch_size = 64
+        dataloaders = load_dataset(len_sequence=len_sequence, batch_size=batch_size, val_ratio=0.1)
+        model = model.to(device)
+        model_ft, hist = train_model(model,
+                               dataloaders,
+                               nn.MSELoss(),
+                               torch.optim.Adam(model.parameters(), lr=0.001),
+                               num_epochs=num_epochs)
+        torch.save(model_ft.state_dict(), model_path)
+
+        plt.xlabel("Training Epochs")
+        plt.ylabel("Validation Accuracy")
+        plt.plot(range(1, num_epochs + 1), hist)
+        plt.ylim((0,1.))
+        plt.xticks(np.arange(1, num_epochs + 1, 1.0))
+        plt.legend()
+        plt.show()
+    elif option == 'run':
+        if len(sys.argv) > 2:
+            video_path = sys.argv[2]
+        else:
+            print('specify a video soruce path')
+            exit()
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+
+        video = cv2.VideoCapture(src_path)
+        fwidth = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        fheight = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = video.get(cv2.CAP_PROP_FPS)
+        num_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        duration = int(num_frames / fps) * 1000
+        interval_millis = 1000
+
+        cur_frame = 0
+        frames = []
+        while(True):
+            if cur_frame > num_frames:
+                break
+            if cur_frame * interval_millis > duration:
+                break
+            ret, frame = video.read()
+            if ret:
+                frame = cv2.resize(frame, dsize=(16 * 14, 9 * 14))
+                if len(frames) == len_sequence:
+                    frames.pop(0)
+                    frames.append(frame)
+                    transpoed_frames = np.asarray([frame.transpose((2, 0, 1)) for frame in frames])
+                    inputs = torch.from_numpy(transpoed_frames).float()
+                    inputs = inputs.to(device)
+                    outputs = model(inputs)
+                    print('time: {} ms, output: {}'.format(cur_frame * interval_millis, outputs))
+                else:
+                    frames.append(frame)
